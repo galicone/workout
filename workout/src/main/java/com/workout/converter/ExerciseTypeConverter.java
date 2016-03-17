9eb40a62a8dcceeb13076e9be8583541a488ed4a
Miroslav <najsnazan@gmail.com>
2016-03-17 22:54:49 +0100

    aaa

---
+++ workout/src/main/java/com/workout/converter/ExerciseTypeConverter.java
@@ -0,0 +1,19 @@
+package com.workout.converter;
+
+import org.springframework.stereotype.Component;
+
+import com.workout.domain.ExerciseType;
+import com.workout.dto.ExerciseTypeDto;
+
+@Component
+public class ExerciseTypeConverter {
+
+	public ExerciseTypeDto convertToDto(ExerciseType exerciseType) {
+		ExerciseTypeDto exerciseTypeDtO = new ExerciseTypeDto();
+		
+		exerciseTypeDtO.setName(exerciseType.getName());
+		exerciseTypeDtO.setDescription(exerciseType.getDescription());
+		
+		return exerciseTypeDtO;
+	}
+}
